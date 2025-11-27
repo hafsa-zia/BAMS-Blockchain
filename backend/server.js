0fad7290377e5cdfdf408558c6bd61585c9c9d0a
hafsa-ziaa <hafsaz2533@gmail.com>
2025-11-27 08:39:53 +0500

    Fix CORS for Vercel againn

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,5 @@
-// backend/server.js
 require("dotenv").config();
 const app = require("./app");
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running locally on port ${PORT}`);
-});
+module.exports = app;
